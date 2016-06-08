2c65ebe23ce89b3e302ecd167b6c6c658b0c2c92
Randy Shaddach <vonrandy@gmail.com>
2016-06-07 20:23:29 -0400

    initial

---
+++ smartapps/rjshadd/smart-home-monitor-actions.src/smart-home-monitor-actions.groovy
@@ -0,0 +1,56 @@
+/**
+ *  Smart Home Monitor Actions
+ *
+ *  Copyright 2016 Randy Shaddach
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+definition(
+    name: "Smart Home Monitor Actions",
+    namespace: "rjshadd",
+    author: "Randy Shaddach",
+    description: "Perform various actions when the Smart Home Monitor changes state",
+    category: "Convenience",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+
+preferences {
+	page(name: "mainPage", title: "Smart Home Monitor Actions", uninstall: true, install: true) {
+        section("SHM Set to Away") {
+        	
+            input "awayMode", "mode", title: "mode when away", multiple: false, required: false
+            input "awaySwitchOn", "capability.switch", title: "turn these switches on", multiple: true, required: false
+            input "awaySwitchOff", "capability.switch", title: "turn these switches off", multiple: true, required: false
+            
+        }
+    }
+}
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+
+	initialize()
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+
+	unsubscribe()
+	initialize()
+}
+
+def initialize() {
+	// TODO: subscribe to attributes, devices, locations, etc.
+}
+
+// TODO: implement event handlers
